2ee3c08befa4cd52ab5b55fd0983f2592e29a778
Sam <jto.sam@gmail.com>
2025-07-12 19:02:54 +0100

    Remove existing policies.json before copying new one in installer script

--- brave-bullshitinator-linux-install.sh
+++ brave-bullshitinator-linux-install.sh
@@ -5,6 +5,7 @@ echo "Brave debullshitinator is made by Mules Gaming. Find it here: https://gith
 sudo mkdir /etc/brave/policies/managed -p
 # Download file
 wget https://raw.githubusercontent.com/samtowers/brave-debloatinator/main/policies.json
+sudo rm /etc/brave/policies/managed/policies.json
 sudo cp policies.json /etc/brave/policies/managed
 rm policies.json
 # End
